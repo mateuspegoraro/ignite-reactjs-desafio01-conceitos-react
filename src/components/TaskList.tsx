301fac6378c80209639c3def8527cb18e5db926a
Mateus <mateus.pegoraro@perceptvision.com.br>
2021-09-12 10:27:52 -0300

    Done

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,34 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle){
+      const newTask: Task = {
+        id: Math.random(),
+        isComplete: false,
+        title: newTaskTitle,
+      }
+  
+      setTasks([...tasks,newTask]);
+      setNewTaskTitle('');
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const taskArray: Task[] = tasks.map(task => task.id === id ? 
+      {
+        ...task,
+        isComplete: !task.isComplete
+      } : task
+    )
+    
+    setTasks([...taskArray]);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const taskArray: Task[] = tasks.filter(t => t.id !== id);
+    setTasks([...taskArray]);
   }
 
   return (
